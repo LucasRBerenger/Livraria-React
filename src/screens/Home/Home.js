eab858811756972237118ae9bc222a8dfe7a59b2
LucasRBerenger <lucasrbnug@gmail.com>
2024-07-06 19:41:46 -0300

    Adjusting home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,43 +1,49 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Card/Card";
 import { Formulario } from "../../components/Formulario/Formulario";
 import { Header } from "../../components/Header/Header";
+import { getLivros } from "../../services/api";
 import "./Home.css";
 
-/*import image1 from "../../assets/";
-import image2 from "../../assets/";
-import image3 from "../../assets/";*/
-
-import { useEffect, useState } from "react";
-import { getLivros } from "../../services/api";
-
 function Home() {
-  const [livros, setLivros] = useState();
+  const [livros, setLivros] = useState([]);
   const [refresh, setRefresh] = useState(false);
 
   useEffect(() => {
-    //getLivrosApi();
-    //console.log(livros)
-    setRefresh(true);
+    getLivrosApi();
+    setRefresh(false);
   }, []);
 
   useEffect(() => {
-    if (refresh)
-      getLivrosApi().then((result) =>
-        setLivros(separaLivroPorCategoria(result))
-      );
+    if (refresh) {
+      getLivrosApi().then((result) => {
+        if (result) {
+          setLivros(separaLivroPorCategoria(result));
+        } else {
+          setLivros([]);
+        }
+      });
+    }
   }, [refresh]);
 
   const getLivrosApi = async () => {
-    await getLivros().then((result) => result);
-    //console.log("abc");
+    const result = await getLivros();
+    return result;
   };
 
   const separaLivroPorCategoria = (livros) => {
-    const categoriaa = [];
+    if (!livros || !Array.isArray(livros)) {
+      return [];
+    }
+
+    const categoriaa = {};
+
     for (const livro of livros) {
       const tipoNome = livro.categoria;
 
-      if (!categoriaa.hasOwnProperty(tipoNome)) categoriaa[tipoNome] = [];
+      if (!categoriaa.hasOwnProperty(tipoNome)) {
+        categoriaa[tipoNome] = [];
+      }
 
       categoriaa[tipoNome].push(livro);
     }
@@ -46,7 +52,7 @@ function Home() {
 
     for (const categoria in categoriaa) {
       resultado.push({
-        tipo: categoria, // <-------- tipo é o correto e não categoria lá no card
+        tipo: categoria,
         livros: categoriaa[categoria],
       });
     }
@@ -63,17 +69,10 @@ function Home() {
         <Formulario setRefresh={setRefresh} />
       </div>
       <div className="div-card">
-        {refresh &&
-          livros &&
-          livros.map((value, index) => {
-            return (
-              <Card
-                key={index}
-                categoria={value.tipo} // As categorias não estavam aparecendo, foi pq eu coloquei categoria ao invés de tipo
-                livros={value.livros}
-              />
-            );
-          })}
+        {livros &&
+          livros.map((value, index) => (
+            <Card key={index} categoria={value.tipo} livros={value.livros} />
+          ))}
       </div>
     </div>
   );
